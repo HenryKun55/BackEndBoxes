896d0046c7680ff4237c69988a8018be9c2607c8
Flávio <henriqueflavio55@gmail.com>
2019-04-11 02:41:03 -0300

    update File.js

--- src/models/File.js
+++ src/models/File.js
@@ -19,9 +19,9 @@ const File = new mongoose.Schema(
 )
 
 File.virtual('url').get(function() {
-    const url = process.env.URL || 'http://localhost:3333'
+    const url = process.env.URL || 'http://localhost:3333/'
 
-    return `${url}/files/${encodeURIComponent(this.path)}`
+    return `${url}files/${encodeURIComponent(this.path)}`
 })
 
 module.exports = mongoose.model("File", File)
